677dcf26e178f3d96feda4724876a090364cc713
Hacker-C <1507559148@qq.com>
2022-05-04 10:15:29 +0800

    💡 Add comment

--- src/easy/00189-easy-awaited/template.ts
+++ src/easy/00189-easy-awaited/template.ts
@@ -6,4 +6,7 @@ type MyAwaited<P extends Promise<unknown>> = P extends Promise<infer X>
   : never
 
 // infer X: 不知道 X 具体是啥，后续操作可能用到，就使用 infer 操作符
+// 1、infer 只能用在 条件类型里面
+// 2、设置一个未知数、变量
+
 // 类型体操真是一门玄学。。
