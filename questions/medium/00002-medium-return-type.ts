93ffb8499139fc2eec3c2eb98ba2c0b74b2a8179
Hacker-C <1507559148@qq.com>
2022-07-31 15:44:57 +0800

    🎉 feat: Finish #2

---
+++ questions/medium/00002-medium-return-type.ts
@@ -0,0 +1,34 @@
+/*
+  2 - Get Return Type  
+  > View on GitHub: https://tsch.js.org/2
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyReturnType<FuncType extends (...args: any) => any> = FuncType extends (
+  ...args: any
+) => infer RetType
+  ? RetType
+  : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<string, MyReturnType<() => string>>>,
+  Expect<Equal<123, MyReturnType<() => 123>>>,
+  Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
+  Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
+  Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>
+]
+
+type ComplexObject = {
+  a: [12, 'foo']
+  bar: 'hello'
+  prev(): number
+}
+
+const fn = (v: boolean) => (v ? 1 : 2)
+const fn1 = (v: boolean, w: any) => (v ? 1 : 2)
