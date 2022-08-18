41583a1b11fb2703fd95d932bc9b402ea2d134da
Hacker-C <1507559148@qq.com>
2022-08-18 15:47:08 +0800

    ✨ feat: Finish #612

---
+++ questions/medium/00612-medium-kebabcase.ts
@@ -0,0 +1,28 @@
+/*
+  612 - KebabCase
+  > View on GitHub: https://tsch.js.org/612
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// 第一次做，判断 First 去了，导致写的很复杂。看了眼解答，醍醐灌顶。
+type KebabCase<S> = S extends `${infer First}${infer Rest}`
+  ? Rest extends Uncapitalize<Rest>
+    ? `${Lowercase<First>}${KebabCase<Rest>}`
+    : `${Lowercase<First>}-${KebabCase<Rest>}`
+  : S
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>
+]
